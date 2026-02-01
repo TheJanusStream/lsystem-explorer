bb86859b03fef7d4c953d06644abf4180bc1efd6
TheJanusStream <thejanusstream@gmail.com>
2026-02-01 18:21:19 +0100

    Simplify lighting

--- src/visuals/scene.rs
+++ src/visuals/scene.rs
@@ -5,9 +5,7 @@ use bevy::prelude::*;
 use bevy_panorbit_camera::PanOrbitCamera;
 
 pub fn setup_scene(mut commands: Commands) {
-    // 3-Point Lighting Setup
-
-    // 1. Key Light (Warm, Bright, Shadow casting) - Main illumination
+    // Directional Light (Sunlight)
     commands.spawn((
         DirectionalLight {
             illuminance: 8000.0,
@@ -20,40 +18,6 @@ pub fn setup_scene(mut commands: Commands) {
             // Angled down and slightly from the side
             rotation: Quat::from_rotation_x(-std::f32::consts::PI / 4.)
                 .mul_quat(Quat::from_rotation_y(-std::f32::consts::PI / 6.)),
-            ..default()
-        },
-    ));
-
-    // 2. Fill Light (Cool, Dimmer, No shadows) - Fills dark areas
-    commands.spawn((
-        DirectionalLight {
-            illuminance: 3000.0,
-            shadows_enabled: false,
-            color: Color::srgb(0.8, 0.85, 1.0), // Cool skylight
-            ..default()
-        },
-        Transform {
-            translation: Vec3::new(-50.0, 50.0, 50.0),
-            // Opposite side to Key light
-            rotation: Quat::from_rotation_x(-std::f32::consts::PI / 3.)
-                .mul_quat(Quat::from_rotation_y(std::f32::consts::PI / 2.)),
-            ..default()
-        },
-    ));
-
-    // 3. Rim Light (Bright, Backlight) - Separates object from background
-    commands.spawn((
-        DirectionalLight {
-            illuminance: 5000.0,
-            shadows_enabled: false,
-            color: Color::srgb(1.0, 1.0, 1.0),
-            ..default()
-        },
-        Transform {
-            translation: Vec3::new(0.0, 50.0, -100.0),
-            // Backlighting
-            rotation: Quat::from_rotation_x(-std::f32::consts::PI / 2.0)
-                .mul_quat(Quat::from_rotation_y(std::f32::consts::PI)),
             ..default()
         },
     ));
